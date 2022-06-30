0ade37e93109c8defea2a3673f31f466f55bc425
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-06-30 14:48:36 +0530

    Add files via upload

---
+++ FibonacciSeries.cs
@@ -0,0 +1,28 @@
+﻿using System; //Only this is in use
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Question: Print the fibonacci series
+namespace QandA
+{
+    class FibonacciSeries
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 1;
+            int c;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            for (int i = 0; i <= 10; i++)
+            {
+                c = a + b;
+                Console.WriteLine(c);
+                a = b;
+                b = c;
+            }
+            Console.ReadLine();
+        }
+    }
+}
