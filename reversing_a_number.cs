6ca54e5ce0ff5d41f075cfee0ce486711e7fa95c
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-06-30 20:49:45 +0530

    Add files via upload

---
+++ reversing_a_number.cs
@@ -0,0 +1,27 @@
+﻿using System; //Only this is in use
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Question: Reverse a number which is entered by the user
+namespace QandA
+{
+    class ReversingANumber
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to reverse it: ");
+            int num = int.Parse(Console.ReadLine());
+            int result = 0;
+            int rem;
+            while(num != 0)
+            {
+                rem = num % 10;
+                result = result * 10 + rem;
+                num = num / 10;
+            }
+            Console.WriteLine("Reversed number is: " + result);
+            Console.ReadLine();
+        }
+    }
+}
