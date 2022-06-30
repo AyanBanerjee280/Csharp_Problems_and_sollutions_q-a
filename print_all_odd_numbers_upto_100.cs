0c3909b0ebfb82d0bdb640975e4f416bc53a3159
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-06-30 10:07:48 +0530

    Add files via upload

---
+++ print_all_odd_numbers_upto_100.cs
@@ -0,0 +1,27 @@
+﻿using System; //Only this is in use
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odd_even_num
+{
+    class print_all_odd_numbers_upto_100
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
