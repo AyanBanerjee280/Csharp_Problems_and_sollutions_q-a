84ed809ca4c441736b6b9b7b70fdbfcbd4d624d6
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-06-30 13:38:15 +0530

    Add files via upload

---
+++ FindLargerNum.cs
@@ -0,0 +1,24 @@
+﻿using System; //Only this is in use
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Question: Find the greater one between two numbers
+namespace odd_even_num
+{
+    class FindLargerNum
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int b = int.Parse(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine("{0} is the greater number.", a);
+            }
+            Console.ReadLine();
+        }
+    }
+}
