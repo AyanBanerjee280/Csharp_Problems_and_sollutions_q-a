d8c818bea5a93d22ca16f7e90a1bae781d415554
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-06-30 10:01:51 +0530

    Add files via upload

---
+++ Finding_Odd_or_Even_Number.cs
@@ -0,0 +1,27 @@
+﻿using System; //Only this is in use
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finding_odd_or_even_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number to check if it is Even or Odd: ");//getting user input
+            int number = int.Parse(Console.ReadLine()); //DataType Conversion
+
+            if (number % 2 == 0) //condition1
+            {
+                Console.WriteLine("This is an even number");
+            }
+            else //condition2
+            {
+                Console.WriteLine("This is an odd number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
