6f0faeec51f0034d04a2b7c84942be1aa9c16034
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-06-30 15:28:45 +0530

    Add files via upload

---
+++ Factorial.cs
@@ -0,0 +1,25 @@
+﻿using System; //Only this is in use
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Question: Find factorial
+namespace QandA
+{
+    class Factorial
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number here: ");
+            int num = int.Parse(Console.ReadLine());
+            int fact = 1;
+
+            for (int i = 0; i <= num; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("The factorial is " + fact + ".");
+            Console.ReadLine();
+        }
+    }
+}
