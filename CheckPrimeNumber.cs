b1e95291d10947eaf82fe964bcfca2a094f5890e
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-01 12:26:37 +0530

    Add files via upload

---
+++ CheckPrimeNumber.cs
@@ -0,0 +1,42 @@
+﻿using System; //Only this is in use
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Question: Check whether the number entered by the user is a Prime Number or not.
+namespace QandA
+{
+    class CheckPrime
+    {
+        static void Main(string[] args)
+        {
+            string value;
+            do
+            {
+                Console.Write("Enter a number here: ");
+                int num = int.Parse(Console.ReadLine());
+                int count = 0;
+                for (int i = 1; i <= num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 2)
+                {
+                    Console.WriteLine("{0} is a Prime Number.", num);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is not a Prime Number.", num);
+                }
+                Console.WriteLine("Press y to continue or any other key to exit ");
+                value = Console.ReadLine();
+            }
+            while (value == "Y" || value == "y");
+
+        }
+    }
+}
+
