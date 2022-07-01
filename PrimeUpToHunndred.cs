51bfecb6e25855e8c2a437b1eedc8a488f268d73
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-01 12:50:14 +0530

    Add files via upload

---
+++ PrimeUpToHunndred.cs
@@ -0,0 +1,35 @@
+﻿using System; //Only this is in use
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Question: Print all Prime numbers up to 100
+namespace QandA
+{
+    class PrimeUpToHundred
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 1; number <= 100; number++)
+            {
+                int count = 0;
+                for (int i = 1; i <= number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 2)
+                {
+                    Console.WriteLine(number);
+                }
+
+            }
+            Console.ReadLine();
+        }
+            
+        
+    }
+}
+
