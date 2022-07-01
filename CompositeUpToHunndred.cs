1753d7cebf7f5663650a763e74b8da59ff427275
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-01 12:51:25 +0530

    Add files via upload

---
+++ CompositeUpToHunndred.cs
@@ -0,0 +1,39 @@
+﻿using System; //Only this is in use
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Question: Print all Composite numbers up to 100
+namespace QandA
+{
+    class CompositeUpToHundred
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 1; number <= 100; number++)
+            {
+                int count = 0;
+                for (int i = 1; i <= number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 2)
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }
+
+            }
+            Console.ReadLine();
+        }
+            
+        
+    }
+}
+
